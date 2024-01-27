46c9e7f3f7072346f015cd441cbc082614981619
3M-Develop <75451081+3MDevelop@users.noreply.github.com>
2024-01-27 13:09:30 +0330

    prepare for product

--- src/components/FooterSection/index.js
+++ src/components/FooterSection/index.js
@@ -1,24 +1,33 @@
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 
 
-const FooterSection = ({handlePageChange}) => {
-    const pagehandler = () => {
-        console.info('page changed')
-    }
+const FooterSection = ({ handlePageChange }) => {
     return (
         <div>
             <div className='appFooter d-flex flex-row-reverse px-1 py-1 text-white'>
                 <div>
-                    <FontAwesomeIcon icon="fa-solid fa-home" onClick={() => { handlePageChange('home') }} />
+                    <FontAwesomeIcon
+                        icon="fa-solid fa-home"
+                        onClick={() => { handlePageChange('home') }}
+                    />
                 </div>
                 <div>
-                    <FontAwesomeIcon icon="fa-solid fa-user" onClick={() => { handlePageChange('profile') }} />
+                    <FontAwesomeIcon
+                        icon="fa-solid fa-user"
+                        onClick={() => { handlePageChange('profile') }}
+                    />
                 </div>
                 <div>
-                    <FontAwesomeIcon icon="fa-solid fa-comment-medical" onClick={() => { handlePageChange('chat') }} />
+                    <FontAwesomeIcon
+                        icon="fa-solid fa-comment-medical"
+                        onClick={() => { handlePageChange('chat') }}
+                    />
                 </div>
                 <div>
-                    <FontAwesomeIcon icon="fa-solid fa-users" onClick={() => { handlePageChange('patientsList') }} />
+                    <FontAwesomeIcon
+                        icon="fa-solid fa-users"
+                        onClick={() => { handlePageChange('patientsList') }}
+                    />
                 </div>
             </div>
         </div>
